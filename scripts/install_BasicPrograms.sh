c94d37bdaf8fa190b4927f66f5aca4f5ce782e1e
SfikasTeo <theodore.sfikas@toolsforumanity.com>
2024-01-20 16:38:12 +0100

    small refactoring & deleted some files

--- scripts/install_BasicPrograms.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-sudo pacman -S btop bottom qtile rofi zsh picom xlip kitty dunst
-sudo pacman -S helix clangd python-lsp-server
-yay -S visual-studio-code-bin codelldb brave-bin masterpdfeditor-free
